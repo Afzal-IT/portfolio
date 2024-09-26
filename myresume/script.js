236ecefcb8006062ac976233983a3032eb56d71f
Afzal-IT <afzal201104@gmail.com>
2024-09-26 21:36:24 +0530

    Add files via upload

---
+++ myresume/script.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementById("dark-mode-toggle");
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+
+
+
+
+
+
+
+
+
